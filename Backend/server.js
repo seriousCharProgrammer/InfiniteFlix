f6f1e87b020ab164a5a1f25a307ff2a67e26a703
seriousCharProgrammer <charbelalhajj81@gmail.com>
2024-12-09 10:49:28 +0200

    done last

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ const limiter = rateLimit({
 });
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // replace with your frontend URL
+  origin: ['https://infiniteflix.onrender.com'], // replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be sent and received
